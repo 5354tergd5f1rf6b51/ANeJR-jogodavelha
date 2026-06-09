fe950c389d5d9985eb4e9988b645ee89192d6b24
Juan Emanuel Rusque Gomes de Menezes <juanrusque08@gmail.com>
2026-06-08 22:11:34 -0300

    Acrescentando especificação de vitória

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Scoreboard from './scoreboard';
 import './styles.css'; // Conecta os estilos que estão na mesma pasta
 
 const WINNING_COMBINATIONS = [
-  [0, 1, 2], [3, 4, 5], [6, 7, 8], // Linhas
-  [0, 3, 6], [1, 4, 7], [2, 5, 8], // Colunas
-  [0, 4, 8], [2, 4, 6]             // Diagonais
+  [0, 1, 2], [3, 4, 5], [6, 7, 8], // Linhas _
+  [0, 3, 6], [1, 4, 7], [2, 5, 8], // Colunas |
+  [0, 4, 8], [2, 4, 6]             // Diagonais \
 ];
 
 export default function App() {
